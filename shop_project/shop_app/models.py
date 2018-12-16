7a9c3e846d6c3ce1b263d3cc41c94f50fbb44c6f
benjamin Cohana <benjamin.cohana@gmail.com>
2018-12-16 15:13:53 +0200

    add image and formulary

--- shop_project/shop_app/models.py
+++ shop_project/shop_app/models.py
@@ -4,7 +4,7 @@ class Product(models.Model):
   name = models.CharField(max_length=264)
   price = models.DecimalField(max_digits=5, decimal_places=2)
   description = models.TextField()
-  image = models.ImageField(default='static/images/a.jpeg', upload_to='static/images/')
+  image = models.ImageField(default='static/images/1.jpeg', upload_to='static/images/shoes/')
 
   def __str__(self):
     return self.name
@@ -27,6 +27,21 @@ class Client(models.Model):
     return "<Client {}>".format(self.email)
 
 
+
+class Comment(models.Model):
+  username = models.CharField(max_length=264)
+  text = models.TextField(max_length=264)
+  date = models.DateField()
+  product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.username
+
+  def __repr__(self):
+    return "<Comment {}>".format(self.username)
+
+
+    
 
 
 
--- shop_project/shop_app/views.py
+++ shop_project/shop_app/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
-from shop_app.models import Product, Client
+from shop_app.models import Product, Client, Comment
+from shop_app.forms import CommentForm
+import datetime
+
 
 def index(request):
-	products = Product.objects.all()
+	products = Product.objects.all()[:20]
 	return render(request,'index.html',{'products':products})
 
 def product(request, product_id):
 	product = Product.objects.get(id=product_id)
-	return render(request,'product.html',{'product':product})
+	comments = Comment.objects.all().filter(product_id=product_id)
+	return render(request,'product.html',{'product':product,'comments':comments})
 
 def clients(request):
 	clients = Client.objects.all()
@@ -17,4 +21,28 @@ def client(request, client_id):
 	client = Client.objects.get(id=client_id)
 	products = Product.objects.all().filter(client=client_id)
 	return render(request,'client.html',{'client':client, 'products':products})
+
+def comment_form(request,product_id):
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		text = request.POST.get('text')
+		product = Product.objects.get(id=product_id)
+		date = datetime.datetime.now()
+		Comment.objects.get_or_create(username=username,text=text,date=date,product=product)[0]
+		return render(request,'comment_form.html', {'comment_form':CommentForm() })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
